1ccd2fbca99f9545a654668ae165c15aeec7a982
yeyahsky <leah.yeyah@gmail.com>
2016-02-08 12:21:54 +0800

    Added PdfTextParser
    created pdfText class. add implement it's methods to uploader class

--- questionclassifier/src/com/blackparty/questionclassifier/models/Item.java
+++ questionclassifier/src/com/blackparty/questionclassifier/models/Item.java
@@ -1,65 +1,108 @@
 package com.blackparty.questionclassifier.models;
 
-
 public class Item {
-	  private String itemNumber;
-	    private String text;
-	    private int pagenum;
-	    private double xCoordinate;
-	    private double yCoordinate;
+	private String itemNumber;
+	private String filename;
+	private String question;
+	private String text;
+	private int pagenum;
+	private int position;
+	private double xCoordinate;
+	private double yCoordinate;
+	private String category;
 
-	    public Item() {
-	    }
+	public Item() {
+	}
 
-	    public Item(String text,String itemNumber, int pagenum, double xCoordinate, double yCoordinate) {
-	        this.itemNumber = itemNumber;
-	        this.xCoordinate = xCoordinate;
-	        this.yCoordinate = yCoordinate;
-	        this.text = text;
-	        this.pagenum = pagenum;
-	    }
+	public Item(String text, String category, String question,
+			String itemNumber, int pagenum, double xCoordinate,
+			double yCoordinate, int position, String filename) {
+		this.itemNumber = itemNumber;
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+		this.text = text;
+		this.pagenum = pagenum;
+		this.position = position;
+		this.question = question;
+		this.category = category;
+		this.filename = filename;
+	}
 
-	 
+	public int getPosition() {
+		return position;
+	}
 
-	    public String getItemNumber() {
-	        return itemNumber;
-	    }
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	
 
-	    public void setItemNumber(String itemNumber) {
-	        this.itemNumber = itemNumber;
-	    }
-	  
-	    public double getxCoordinate() {
-	        return xCoordinate;
-	    }
-	    public void setxCoordinate(double xCoordinate) {
-	        this.xCoordinate = xCoordinate;
-	    }
+	public String getFilename() {
+		return filename;
+	}
 
-	    public double getyCoordinate() {
-	        return yCoordinate;
-	    }
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
 
-	    public void setyCoordinate(double yCoordinate) {
-	        this.yCoordinate = yCoordinate;
-	    }
+	public void setPagenum(int pagenum) {
+		this.pagenum = pagenum;
+	}
 
-	    public String getText() {
-	        return text;
-	    }
+	public String getItemNumber() {
+		return itemNumber;
+	}
 
-	    public void setText(String text) {
-	        this.text = text;
-	    }
-	    
+	public void setItemNumber(String itemNumber) {
+		this.itemNumber = itemNumber;
+	}
 
-		public int getPagenum() {
-			return pagenum;
-		}
+	public double getxCoordinate() {
+		return xCoordinate;
+	}
 
-		public void setPageNum(int pagenum) {
-			this.pagenum = pagenum;
-		}
-	    
-	    
+	public void setxCoordinate(double xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+
+	public double getyCoordinate() {
+		return yCoordinate;
+	}
+
+	public void setyCoordinate(double yCoordinate) {
+		this.yCoordinate = yCoordinate;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public int getPagenum() {
+		return pagenum;
+	}
+
+	public void setPageNum(int pagenum) {
+		this.pagenum = pagenum;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
 }
